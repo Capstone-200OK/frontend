ce9a55580495a196982d7eeceae9b954765b44ac
hyunseo <eieen120919@gmail.com>
2025-04-09 10:37:39 +0900

    로그인창을 꾸미는 중...

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -21,14 +21,17 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   // 입력 필드에 사용할 컨트롤러
-  final TextEditingController _idController = TextEditingController(); // 아이디 입력 컨트롤러
-  final TextEditingController _emailController = TextEditingController(); // 이메일 입력 컨트롤러
-  final TextEditingController _passwordController = TextEditingController(); // 비밀번호 입력 컨트롤러
+  final TextEditingController _idController =
+      TextEditingController(); // 아이디 입력 컨트롤러
+  final TextEditingController _emailController =
+      TextEditingController(); // 이메일 입력 컨트롤러
+  final TextEditingController _passwordController =
+      TextEditingController(); // 비밀번호 입력 컨트롤러
 
   // 사용자 정보 리스트 (로그인할 때 비교할 데이터)
   List<User> users = [
     User(id: 'yeeun123', email: 'yeeun@naver.com', password: '123456'),
-    User(id: 'hyunseo', email: '111@naver.com', password: '111111'),
+    User(id: '111111', email: '111@naver.com', password: '111111'),
     User(id: 'testUser', email: 'test@example.com', password: 'password123'),
   ];
 
@@ -55,89 +58,111 @@ class _LoginScreenState extends State<LoginScreen> {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(
-          builder: (context) => HomeScreen(username: loggedInUser!.id), // 홈 화면으로 이동
+          builder:
+              (context) => HomeScreen(username: loggedInUser!.id), // 홈 화면으로 이동
         ),
       );
       // 로그인 성공 메시지 표시
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('로그인 성공')));
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text('로그인 성공')));
     } else {
       // 로그인 실패 시 오류 메시지 표시
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('회원가입이 안 되어 있거나 로그인 정보가 일치하지 않습니다.')));
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('회원가입이 안 되어 있거나 로그인 정보가 일치하지 않습니다.')),
+      );
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white, // 화면 배경 색상 흰색
+      backgroundColor: Color(0xFF263238), // 화면 배경 색상 어두운 색
       appBar: AppBar(
-        title: const Text(
-          '로그인', // AppBar의 제목 텍스트
-          style: TextStyle(fontSize: 15), // 제목 글씨 크기 설정
+        centerTitle: true,
+        toolbarHeight: 210, // AppBar 높이 늘려서
+
+        title: Image.asset(
+          'assets/images/LOGO-text2.png', // 이미지 경로
+          height: 400, // 이미지 크기 조정
         ),
-        backgroundColor: Colors.white, // AppBar 배경색 흰색
+        backgroundColor: const Color(0xFF263238), //백그라운 컬러러
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0), // 화면의 여백 설정
+        padding: const EdgeInsets.symmetric(
+          horizontal: 280.0,
+          vertical: 10,
+        ), // 위젯, 화면 사이 여백
         child: ListView(
           children: [
             // 아이디 입력 필드
             TextFormField(
               controller: _idController, // 아이디 입력을 위한 컨트롤러 연결
+              style: const TextStyle(color: Colors.white, fontSize: 13),
               decoration: const InputDecoration(
                 labelText: '아이디', // 필드 라벨 텍스트
+                labelStyle: const TextStyle(color: Colors.white, fontSize: 12),
                 border: OutlineInputBorder(), // 테두리 설정
               ),
             ),
-            const SizedBox(height: 20), // 입력 필드 간 간격 설정
-
+            const SizedBox(height: 13), // 입력 필드 간 간격 설정
             // 이메일 입력 필드
             TextFormField(
               controller: _emailController, // 이메일 입력을 위한 컨트롤러 연결
+              style: const TextStyle(color: Colors.white, fontSize: 13),
               decoration: const InputDecoration(
                 labelText: '이메일', // 필드 라벨 텍스트
+                labelStyle: const TextStyle(color: Colors.white, fontSize: 12),
                 border: OutlineInputBorder(), // 테두리 설정
               ),
               keyboardType: TextInputType.emailAddress, // 이메일 형식 키보드 타입 설정
             ),
-            const SizedBox(height: 20), // 입력 필드 간 간격 설정
-            
+            const SizedBox(height: 13), // 입력 필드 간 간격 설정
             // 비밀번호 입력 필드
             TextFormField(
               controller: _passwordController, // 비밀번호 입력을 위한 컨트롤러 연결
+              style: const TextStyle(color: Colors.white, fontSize: 13),
               decoration: const InputDecoration(
                 labelText: '비밀번호', // 필드 라벨 텍스트
+                labelStyle: const TextStyle(color: Colors.white, fontSize: 12),
                 border: OutlineInputBorder(), // 테두리 설정
               ),
               obscureText: true, // 비밀번호는 가려서 표시
             ),
-            const SizedBox(height: 20), // 입력 필드 간 간격 설정
-
+            const SizedBox(height: 13), // 입력 필드 간 간격 설정
             // 로그인 버튼
             ElevatedButton(
               onPressed: _login, // 로그인 버튼 클릭 시 _login 함수 호출
               child: const Text('로그인'), // 버튼 텍스트
               style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.grey[300]), // 연한 회색 배경
-                foregroundColor: MaterialStateProperty.all(Colors.black), // 버튼 글자 색상 검정색
+                backgroundColor: MaterialStateProperty.all(
+                  Colors.grey[300],
+                ), // 연한 회색 배경
+                foregroundColor: MaterialStateProperty.all(
+                  Colors.black,
+                ), // 버튼 글자 색상 검정색
               ),
             ),
             const SizedBox(height: 20), // 버튼 간 간격 설정
-
             // 회원가입 화면으로 이동하는 버튼
-            ElevatedButton(
+            TextButton(
               onPressed: () {
                 // 회원가입 화면으로 이동
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => const SignUpScreen()), // 회원가입 화면으로 전환
+                  MaterialPageRoute(
+                    builder: (context) => const SignUpScreen(),
+                  ), // 회원가입 화면으로 전환
                 );
               },
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.grey[300]), // 연한 회색 배경
-                foregroundColor: MaterialStateProperty.all(Colors.black), // 버튼 글자 색상 검정색
+              child: const Text(
+                '회원가입',
+                style: TextStyle(
+                  color: Colors.white, 
+                  fontSize: 12,
+                  decoration: TextDecoration.underline, // 밑줄 추가해서 클릭 가능한 느낌
+                ),
               ),
-              child: const Text('회원가입'), // 버튼 텍스트
             ),
           ],
         ),
