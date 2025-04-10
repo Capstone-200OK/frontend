9d0ec46ef93dbd00ac5edb55777f3fe7ab70f3e0
hyunseo <eieen120919@gmail.com>
2025-04-10 12:15:55 +0900

    개인홈꾸미는중중

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/screens/login_screen.dart'; // 로그인 화면 불러오기
+//import 'package:flutter_application_1/screens/login_screen.dart'; // 로그인 화면 불러오기
+import 'package:flutter_application_1/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LoginScreen(), // 로그인 화면을 초기 화면으로 설정
+      //home: const LoginScreen(), // 로그인 화면을 초기 화면으로 설정
+      home: const HomeScreen(username: '현서'),
     );
   }
 }
