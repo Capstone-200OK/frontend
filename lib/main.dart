df0094d96ef7dfe8c3db7ab8f4e6e7bc5da959b5
hyunseo <eieen120919@gmail.com>
2025-04-17 04:03:33 +0900

    folder_create

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter_application_1/screens/home_screen.dart'; // 홈홈 화면 불러오기
+//import 'package:flutter_application_1/screens/home_screen.dart'; // 홈홈 화면 불러오기
 import 'models/counter_model.dart'; // provider 모델 import
-
+import 'package:flutter_application_1/screens/folder_create.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 //import 'package:flutter_application_1/screens/login_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();  // 1번코드
+  await dotenv.load(fileName: ".env");    // 2번코드
   runApp(const MyApp());
 }
 
@@ -19,7 +22,8 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Flutter 로그인 앱',
         theme: ThemeData(primarySwatch: Colors.blue),
-        home: const HomeScreen(username: '현서'),
+        //home: const HomeScreen(username: '현서'),
+        home: const FolderCreateScreen(),
       ),
     );
   }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -44,7 +44,7 @@ class _LoginScreenState extends State<LoginScreen> {
     String email = _emailController.text;
     String password = _passwordController.text;
     
-    final url = Uri.parse("http://223.194.131.145:8080/user/login");
+    //final url = Uri.parse("http://223.194.131.101.145:8080/user/login");
 
     try {
       final response = await http.post(
