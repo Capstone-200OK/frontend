38249dc5b31d1f36b712b527ec6f907dead0abc7
Seoyeonni <zerordxz@gmail.com>
2025-05-09 00:06:02 +0900

    예약된 자동분류 실행 시 알림 기능 추가

--- lib/main.dart
+++ lib/main.dart
@@ -6,16 +6,28 @@ import 'models/counter_model.dart'; // provider 모델 import
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_application_1/screens/login_screen.dart';
 import 'package:flutter_application_1/providers/user_provider.dart';
+import 'package:flutter_application_1/api/websocket_service.dart';
+import 'package:flutter_application_1/components/notification_button.dart'; // NotificationButton 위젯
+import 'package:provider/provider.dart';
+import 'package:flutter_application_1/providers/notification_provider.dart';
+
+final GlobalKey<ScaffoldMessengerState> rootScaffoldMessengerKey = GlobalKey<ScaffoldMessengerState>();
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+late NotificationProvider globalNotificationProvider;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();  // 1번코드
   await dotenv.load(fileName: ".env");    // 2번코드
   runApp(MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => UserProvider()),
-      ],
-      child: const MyApp(),
-    ),);
+    providers: [
+      ChangeNotifierProvider(create: (_) => UserProvider()),
+      ChangeNotifierProvider(create: (_) {
+        globalNotificationProvider = NotificationProvider();
+        return globalNotificationProvider;
+      }),
+    ],
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -27,6 +39,7 @@ class MyApp extends StatelessWidget {
       create: (_) => CounterModel(),
       child: MaterialApp(
         title: 'Flutter 로그인 앱',
+        scaffoldMessengerKey: rootScaffoldMessengerKey,
         theme: ThemeData(primarySwatch: Colors.blue),
         //home: const HomeScreen(username: '현서'),
         //home: const FolderCreateScreen(),
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,7 +9,10 @@ import 'dart:convert'; // for jsonDecode
 import 'package:http/http.dart' as http; // for http.get
 import 'package:flutter_dotenv/flutter_dotenv.dart'; // for dotenv.get
 import 'package:flutter_application_1/components/search_bar_with_overlay.dart';
-
+import 'package:flutter_application_1/api/websocket_service.dart';
+import 'package:flutter_application_1/components/notification_button.dart'; // NotificationButton 위젯
+import 'package:provider/provider.dart';
+import 'package:flutter_application_1/providers/notification_provider.dart';
 
 class HomeScreen extends StatefulWidget {
   final String username;
@@ -20,9 +23,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final TextEditingController _searchController = TextEditingController();
-  List<Map<String, dynamic>> searchResults = [];
-  OverlayEntry? _searchOverlay;
   late int? userId;
   late String url;
 
@@ -32,129 +32,112 @@ class _HomeScreenState extends State<HomeScreen> {
     WidgetsBinding.instance.addPostFrameCallback((_) {
       userId = Provider.of<UserProvider>(context, listen: false).userId;
       url = dotenv.get("BaseUrl");
+      if (userId != null) {
+        WebSocketService().connect(context, userId!);
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // 상단 AppBar 설정
       appBar: AppBar(
         title: Image.asset(
-          'assets/images/LOGO-text.png', //로고 이미지지
-          height: 230, // 이미지 높이 조정
+          'assets/images/LOGO-text.png',
+          height: 230,
         ),
-        //centerTitle: true, // 가운데 정렬 (선택사항)
         backgroundColor: Colors.white,
         actions: [
           Padding(
-            padding: const EdgeInsets.only(right: 111), // 오른쪽에서 10px 떨어짐
+            padding: const EdgeInsets.only(right: 111),
             child: Row(
               children: [
                 IconButton(
                   icon: const Icon(
                     Icons.history,
                     color: Color(0xff263238),
-                  ), //최근항목아이콘
+                  ),
                   onPressed: () {
                     final userId =
-                        Provider.of<UserProvider>(
-                          context,
-                          listen: false,
-                        ).userId;
+                        Provider.of<UserProvider>(context, listen: false).userId;
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder:
-                            (context) => RecentFileScreen(
-                              username: widget.username,
-                              userId: userId,
-                            ),
+                        builder: (context) => RecentFileScreen(
+                          username: widget.username,
+                          userId: userId,
+                        ),
                       ),
                     );
                     print('최근 항목 눌림');
                   },
                 ),
-                IconButton(
-                  icon: const Icon(
-                    Icons.notifications,
-                    color: Color(0xff263238),
-                  ), //d알림 버튼튼
-                  onPressed: () {
-                    print('알림 눌림');
-                  },
-                ),
+                const NotificationButton(),
               ],
             ),
           ),
         ],
       ),
-
       drawer: NavigationDrawerWidget(
         username: widget.username,
-        onFolderCreated: (folderName) {
-          // 필요 시 폴더 생성 후 작업 추가
-        },
-        folders: const [], // 필요시 폴더 목록 전달
+        onFolderCreated: (folderName) {},
+        folders: const [],
         scaffoldContext: context,
         showUploadButton: false,
       ),
-
-      // 화면 내용 부분
       body: Container(
-        color: Colors.white, // 전체 화면 배경색을 흰색으로 설정
-        padding: const EdgeInsets.all(16.0), // 화면 가장자리 여백 설정
+        color: Colors.white,
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
             Align(
-              alignment: Alignment.center, // 글씨를 화면 중앙에 배치
+              alignment: Alignment.center,
               child: Text(
-                '${widget.username}님, 안녕하세요', // 사용자 이름을 동적으로 출력
+                '${widget.username}님, 안녕하세요',
                 style: const TextStyle(
-                  fontSize: 30, // 글씨 크기 설정
+                  fontSize: 30,
                   fontFamily: 'APPLESDGOTHICNEOEB',
-                  color: Colors.black, // 글씨 색상은 검정색
+                  color: Colors.black,
                 ),
               ),
             ),
-            const SizedBox(height: 100), // 요소 간의 간격 설정
-            // 개인, 홈, 클라우드 버튼들이 가로로 배치
+            const SizedBox(height: 100),
             Row(
-              mainAxisAlignment: MainAxisAlignment.center, // 버튼들을 화면 중앙에 배치
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                // 개인 버튼
                 ElevatedButton(
                   onPressed: () {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder:
-                            (context) => PersonalScreen(username: widget.username, targetPathIds: null,),
+                        builder: (context) => PersonalScreen(
+                          username: widget.username,
+                          targetPathIds: null,
+                        ),
                       ),
                     );
                   },
                   style: ElevatedButton.styleFrom(
                     shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20), // 모서리를 둥글게
+                      borderRadius: BorderRadius.circular(20),
                     ),
                     minimumSize: const Size(200, 100),
                     padding: const EdgeInsets.symmetric(
                       horizontal: 50,
                       vertical: 30,
                     ),
-                    backgroundColor: Color(0xFFCFD8DC), //아이콘 색색
+                    backgroundColor: Color(0xFFCFD8DC),
                     foregroundColor: Colors.black,
                   ),
                   child: const Column(
-                    // 아이콘 아래에 텍스트 배치
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Icon(
                         Icons.person,
                         size: 50,
                         color: Color(0xff263238),
-                      ), // 아이콘 삽입
-                      SizedBox(width: 8), // 아이콘과 텍스트 사이 간격
+                      ),
+                      SizedBox(width: 8),
                       Text(
                         '개인',
                         style: TextStyle(
@@ -165,15 +148,15 @@ class _HomeScreenState extends State<HomeScreen> {
                     ],
                   ),
                 ),
-
-                const SizedBox(width: 150), // 버튼들 간의 간격 설정
-                // 클라우드 버튼
+                const SizedBox(width: 150),
                 ElevatedButton(
                   onPressed: () {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => CloudScreen(username: widget.username),
+                        builder: (context) => CloudScreen(
+                          username: widget.username,
+                        ),
                       ),
                     );
                   },
@@ -206,10 +189,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
               ],
             ),
-
-            const SizedBox(height: 237), // 요소 간의 간격 설정
-
-            // 검색창(TextField) 부분
+            const SizedBox(height: 237),
             SearchBarWithOverlay(
               baseUrl: dotenv.get("BaseUrl"),
               username: widget.username,
