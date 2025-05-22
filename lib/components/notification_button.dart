a70a588fb826b737d37ce81d88aa7c0e1a2da194
hyunseo <eieen120919@gmail.com>
2025-05-22 17:27:35 +0900

    수정정

--- lib/components/notification_button.dart
+++ lib/components/notification_button.dart
@@ -16,91 +16,126 @@ class NotificationButton extends StatelessWidget {
               onPressed: () {
                 showDialog(
                   context: context,
-                  builder: (_) => StatefulBuilder(
-                    builder: (context, setState) {
-                      return AlertDialog(
-                        backgroundColor: const Color(0xFFECEFF1),
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                        titlePadding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-                        contentPadding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                        title: Column(
-                          children: [
-                            const Icon(Icons.notifications, color: Color(0xff263238), size: 40),
-                            const SizedBox(height: 8),
-                            const Divider(thickness: 1, color: Colors.black26),
-                          ],
-                        ),
-                        content: provider.notifications.isEmpty
-                            ? const SizedBox(
-                                height: 100,
-                                child: Center(
-                                  child: Text('알림이 없습니다.',
-                                      textAlign: TextAlign.center,
-                                      style: TextStyle(
-                                          fontFamily: 'APPLESDGOTHICNEOR',
-                                          fontSize: 14,
-                                          color: Colors.black54)),
+                  builder:
+                      (_) => StatefulBuilder(
+                        builder: (context, setState) {
+                          return AlertDialog(
+                            backgroundColor: const Color(0xFFECEFF1),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            titlePadding: const EdgeInsets.fromLTRB(
+                              20,
+                              20,
+                              20,
+                              0,
+                            ),
+                            contentPadding: const EdgeInsets.fromLTRB(
+                              20,
+                              10,
+                              20,
+                              10,
+                            ),
+                            title: Column(
+                              children: [
+                                const Icon(
+                                  Icons.notifications,
+                                  color: Color(0xff263238),
+                                  size: 40,
                                 ),
-                              )
-                            : SizedBox(
-                                width: double.maxFinite,
-                                child: ListView.builder(
-                                  shrinkWrap: true,
-                                  itemCount: provider.notifications.length,
-                                  itemBuilder: (context, index) {
-                                    return Column(
-                                      children: [
-                                        ListTile(
-                                          title: Text(
-                                            provider.notifications[index],
-                                            style: TextStyle(
-                                              fontFamily: 'APPLESDGOTHICNEOEB',
-                                              fontSize: 18,
-                                              color: Colors.black,
-                                              ),
-                                          ),
-                                          trailing: IconButton(
-                                            icon: const Icon(Icons.delete, size: 25),
-                                            onPressed: () {
-                                              provider.removeNotification(index);
-                                              setState(() {});
-                                            },
+                                const SizedBox(height: 8),
+                                const Divider(
+                                  thickness: 1,
+                                  color: Colors.black26,
+                                ),
+                              ],
+                            ),
+                            content:
+                                provider.notifications.isEmpty
+                                    ? const SizedBox(
+                                      width: 400,
+                                      height: 100,
+                                      child: Center(
+                                        child: Text(
+                                          '알림이 없습니다.',
+                                          textAlign: TextAlign.center,
+                                          style: TextStyle(
+                                            fontFamily: 'APPLESDGOTHICNEOR',
+                                            fontSize: 14,
+                                            color: Colors.black54,
                                           ),
                                         ),
-                                        const SizedBox(height: 13,),
-                                        const Divider(height: 1, thickness: 0.5),
-                                      ],
-                                    );
-                                  },
-                                ),
-                              ),
-                        actions: [
-                          TextButton(
-                            onPressed: () {
-                              provider.markAllAsRead();
-                              Navigator.pop(context);
-                            },
-                            child: const Text('닫기',
-                                style: TextStyle(
+                                      ),
+                                    )
+                                    : SizedBox(
+                                      width: 400,
+                                      height: 100, // 원하는 높이로 조정 가능
+                                      child: ListView.builder(
+                                        itemCount:
+                                            provider.notifications.length,
+                                        itemBuilder: (context, index) {
+                                          return Column(
+                                            children: [
+                                              ListTile(
+                                                title: Text(
+                                                  provider.notifications[index],
+                                                  style: const TextStyle(
+                                                    fontFamily:
+                                                        'APPLESDGOTHICNEOR',
+                                                    fontSize: 13,
+                                                    color: Colors.black,
+                                                  ),
+                                                ),
+                                                trailing: IconButton(
+                                                  icon: const Icon(
+                                                    Icons.delete,
+                                                    size: 20,
+                                                  ),
+                                                  onPressed: () {
+                                                    provider.removeNotification(
+                                                      index,
+                                                    );
+                                                    setState(() {});
+                                                  },
+                                                ),
+                                              ),
+                                              const SizedBox(height: 13),
+                                              const Divider(
+                                                height: 1,
+                                                thickness: 0.5,
+                                              ),
+                                            ],
+                                          );
+                                        },
+                                      ),
+                                    ),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  provider.markAllAsRead();
+                                  Navigator.pop(context);
+                                },
+                                child: const Text(
+                                  '닫기',
+                                  style: TextStyle(
                                     fontSize: 12,
                                     fontFamily: 'APPLESDGOTHICNEOR',
-                                    color: Color(0xFF596D79))),
-                          )
-                        ],
-                      );
-                    },
-                  ),
+                                    color: Color(0xFF596D79),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          );
+                        },
+                      ),
                 );
-                },
+              },
             ),
             if (provider.hasUnread)
               const Positioned(
                 right: 6,
                 top: 6,
-                child: CircleAvatar(
-                  radius: 5,
-                  backgroundColor: Colors.red,
-                ),
+                child: CircleAvatar(radius: 5, backgroundColor: Colors.red),
               ),
           ],
         );
